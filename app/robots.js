2c21670de375db4f55f6198dc7bfb025a1c41044
MFcv1 <114358633+MFcv1@users.noreply.github.com>
2026-06-30 14:13:51 +0200

    v1.5.5

--- app/robots.js
+++ app/robots.js
@@ -1,6 +1,6 @@
 import { publicEnv } from '../src/lib/server/env';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-static';
 
 export default function robots() {
   const siteUrl = publicEnv.siteUrl.replace(/\/$/, '');
